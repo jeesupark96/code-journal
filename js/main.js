e04c9439ed5803092d4e060d67753cf17ff9bbc6
Jeesu Park <jeesupark96@gmail.com>
2021-12-27 17:23:26 -0800

    entry form 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 /* global data */
 /* exported data */
+var photo = document.getElementById('images');
+var photoUrl = document.getElementById('photo-url');
+function newPhoto(event) {
+  photo.src = photoUrl.value;
+}
+
+document.addEventListener('input', newPhoto);
+
+var form = document.querySelector('form');
+var title = document.getElementById('title-name');
+var notes = document.getElementById('Notes');
+localStorage.setItem(data, data);
+
+function formEntry(event) {
+  event.preventDefault();
+  var objectOne = {
+    title: title.value,
+    PhotoUrl: photoUrl.value,
+    Notes: notes.value,
+    nextEntryId: data.nextEntryId++
+  };
+
+  data.entries.unshift(objectOne);
+  form.reset();
+  photo.src = 'images/placeholder-image-square.jpg';
+}
+document.addEventListener('submit', formEntry);
